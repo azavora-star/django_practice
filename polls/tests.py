502e3fdc3823e16c1b138fe7df3c7d51a0434959
azavora <azavora@gmail.com>
2023-10-07 10:52:51 +0100

    keeping all up to date

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from polls.models import Question
 
-# Create your tests here.
+class QuestionTest(TestCase):
+    def setUp(self):
+        Question.objects.create(question_text="Test Question", pub_date="2023-09-16 07:36:29")
+        
+    def test_orm(self):
+        TestObject = Question.objects.filter(pub_date="2023-09-16 07:36:29").first()
+        text = TestObject.question_text
+        self.assertEqual("Test Question", text)
+# if __name__ == "__main__":
+#     unittest.main()
